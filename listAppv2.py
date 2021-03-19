30d76660782fd7a8932554873720211cdb7b359e
HunterCUBEschool <79872738+HunterCUBEschool@users.noreply.github.com>
2021-03-19 13:14:29 -0600

    Add files via upload

--- listAppv2.py
+++ listAppv2.py
@@ -13,26 +13,32 @@ import random
 myList = []
 def mainProgram():
     while True:
-        print("Hello, there! Let's work with lists!")
-        print("Choose from the following options. Type a number below!")
-        choice = input("""1. Add to a list ,
-    2. Add a bunch of numbers t a list
+        try:
+            print("Hello, there! Let's work with lists!")
+            print("Choose from the following options. Type a number below!")
+            choice = input("""    1. Add to the list ,
+    2. Add a bunch of numbers to the list
     3. Return the value at an index position
     4. Random search
-    5. Print contents of list
-    6. Quit program""")
-        if choice == '1':
-            addToList()
-        elif choice == '2':
-            addABunch()
-        elif choice == '3':
-            indexValues()
-        elif choice == '4':
-            randomSearch()
-        elif choice == '5':
-            print(myList)
-        else:
-            break
+    5. Linear search
+    6. Print contents of list
+    7. Quit program   """)
+            if choice == '1':
+                addToList()
+            elif choice == '2':
+                addABunch()
+            elif choice == '3':
+                indexValues()
+            elif choice == '4':
+                randomSearch()
+            elif choice == '5':
+                linearSearch()
+            elif choice == '6':
+                print(myList)
+            else:
+                break
+        except:
+            print("You found a error! Uh oh.")
         #TO ADD: 1. a way to loop the action, 2. a way to quit, 3. think of repetition
     
 def addToList():
@@ -43,16 +49,16 @@ def addToList():
 def addABunch():
     print("We're gonna add a bunch of integers here!")
     numToAdd = input("how many integers to add?   ")
-    numRange = input("And how high would you like those numbers to go?   ")
+    numBottomRange = input("How low would you like the numbers to go?   ")
+    numTopRange = input("And how high would you like those numbers to go?   ")
     for x in range (0, int(numToAdd)):
-        myList.append(random.randint(0, int(numRange)))
+        myList.append(random.randint(int(numBottomRange), int(numTopRange)))
     print("Your list is now complete.")
     
 def indexValues():
     print("Loading up your list now")
     indexPos = input("Which data piece would you like to see?    ")
-    print(myList[int(indexPos)])
-
+    print("Your search came up with {}".format(myList[int(indexPos)]))
 def randomSearch():
     print("Random search time!")
     print(myList[random.randint(0, len(myList)-1)])
@@ -62,7 +68,7 @@ def linearSearch():
     searchItem = input("What're you looking for?   ")
     for x in range(len(myList)):
         if myList[x] == int(searchItem):
-            print("Your item is at index position ()".format(x))
+            print("Your item is at index position {}".format(x))
     
 
 if __name__ == "__main__" :
